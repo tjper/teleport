fde1896c3098c403dbe905fc79de04d9d849886b
James <tjamesperry@hotmail.com>
2022-03-11 22:34:52 -0600

    cgroup: defined setup and cleanup tests for jobworker cgroups

---
+++ internal/jobworker/cgroups/cgroups_test.go
@@ -0,0 +1,100 @@
+package cgroups
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+func TestCleanup(t *testing.T) {
+	if !isRoot() {
+		t.Skip("must be root to run")
+	}
+
+	service, err := NewService()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if _, err := os.Stat(service.path); err != nil {
+		t.Error(err)
+	}
+
+	if err := service.Cleanup(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if _, err := os.Stat(service.path); !errors.Is(err, fs.ErrNotExist) {
+		t.Errorf("expected cgroup to not exist; path: %s, err: %v", service.path, err)
+		return
+	}
+}
+
+func TestCleanupWithCgroups(t *testing.T) {
+	if !isRoot() {
+		t.Skip("must be root to run")
+	}
+
+	service, err := NewService()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if _, err := service.CreateCgroup(); err != nil {
+		t.Error(err)
+	}
+
+	if err := service.Cleanup(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if _, err := os.Stat(service.path); !errors.Is(err, fs.ErrNotExist) {
+		t.Errorf("expected cgroup to not exist; path: %s, err: %v", service.path, err)
+		return
+	}
+}
+
+func TestCleanupWithPids(t *testing.T) {
+	if !isRoot() {
+		t.Skipf("must be root to run %v", t.Name())
+	}
+
+	service, err := NewService()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	cgroup, err := service.CreateCgroup()
+	if err != nil {
+		t.Error(err)
+	}
+
+	cmd := exec.Command("sleep", "30")
+	if err := cmd.Start(); err != nil {
+		t.Error(err)
+	}
+
+	if err := service.PlaceInCgroup(*cgroup, cmd.Process.Pid); err != nil {
+		t.Error(err)
+	}
+
+	if err := service.Cleanup(); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(service.path); !errors.Is(err, fs.ErrNotExist) {
+		t.Errorf("expected cgroup to not exist; path: %s, err: %v", service.path, err)
+		return
+	}
+}
+
+func isRoot() bool {
+	return os.Getegid() == 0
+}
