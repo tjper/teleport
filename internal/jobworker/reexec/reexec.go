4cc3a1be0e1bc9e09b85a2762175b6ade74a7801
James <tjamesperry@hotmail.com>
2022-03-13 18:57:35 -0500

    job: created reexec package for launching arbitrary command in child process

---
+++ internal/jobworker/reexec/reexec.go
@@ -0,0 +1,152 @@
+package reexec
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+
+	ierrors "github.com/tjper/teleport/internal/errors"
+	"github.com/tjper/teleport/internal/jobworker/output"
+	"github.com/tjper/teleport/internal/log"
+
+	"github.com/google/uuid"
+)
+
+// logger is an object for logging package events to stdout.
+var logger = log.New(os.Stdout, "reexec")
+
+var (
+	// ErrCommandPipeNotFound indicates that the parent process did not properly
+	// configure the command pipe and pass it to the child process.
+	ErrCommandPipeNotFound = errors.New("command pipe not found")
+	// ErrContinuePipeNotFound indicates that the parent process did not properly
+	// configure the continue pipe and pass it to the child process.
+	ErrContinuePipeNotFound = errors.New("continue pipe not found")
+)
+
+var (
+	// errExpectedEOF indicates the read operation expected an io.EOF error, but
+	// no error was returned.
+	errExpectedEOF = errors.New("expected EOF")
+)
+
+const (
+	// CommandSuccess indicates the reexec exuction of a command completed
+	// successfully.
+	CommandSuccess = 0
+	// CommandFailure indicates the reexec execution of a command failed before
+	// being executed; in the setup phase.
+	CommandFailure = 100
+)
+
+type Job struct {
+	ID  uuid.UUID
+	Cmd Command
+}
+
+// Command represents a shell command.
+type Command struct {
+	// Name is the leading name of the command.
+	Name string
+	// Args are the arguments of the command.
+	Args []string
+}
+
+// Exec utilizes the piped data from the parent process to build and run a
+// arbitrary command on the host system.
+func Exec(ctx context.Context) (int, error) {
+	// Parent process has set /proc/self/fd/3 to the command pipe receiver.
+	cmdfd := os.NewFile(uintptr(3), "/proc/self/fd/3")
+	if cmdfd == nil {
+		return CommandFailure, ErrCommandPipeNotFound
+	}
+
+	// Parent process has set the /proc/self/fd/4 to the continue pipe receiver.
+	contfd := os.NewFile(uintptr(4), "/proc/self/fd/4")
+	if contfd == nil {
+		return CommandFailure, ErrContinuePipeNotFound
+	}
+
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(cmdfd); err != nil {
+		return CommandFailure, ierrors.Wrap(err)
+	}
+	var job Job
+	if err := json.Unmarshal(buf.Bytes(), &job); err != nil {
+		return CommandFailure, ierrors.Wrap(err)
+	}
+
+	// Create log file for stdout and stderr output.
+	outfd, err := os.OpenFile(output.File(job.ID), os.O_CREATE|os.O_WRONLY, output.FileMode)
+	if err != nil {
+		return CommandFailure, ierrors.Wrap(err)
+	}
+	defer func() {
+		if err := outfd.Close(); err != nil {
+			logger.Errorf("closing output fd; error: %s", err)
+		}
+	}()
+
+	// Build command to be run on host system.
+	cmd := exec.Command(job.Cmd.Name, job.Cmd.Args...)
+	cmd.Stdout = outfd
+	cmd.Stderr = outfd
+
+	// Wait for continue signal from parent process. This will be sent once
+	// process has been placed in the appropriate cgroup.
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	if err := waitForContinue(ctx, contfd); err != nil {
+		return CommandFailure, ierrors.Wrap(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return CommandFailure, ierrors.Wrap(err)
+	}
+
+	err = cmd.Wait()
+	return exitCode(err), nil
+}
+
+func exitCode(err error) int {
+	if err == nil {
+		return CommandSuccess
+	}
+
+	exitError := new(exec.ExitError)
+	if errors.As(err, &exitError) {
+		status, ok := exitError.Sys().(syscall.WaitStatus)
+		if !ok {
+			return CommandFailure
+		}
+		return status.ExitStatus()
+	}
+	return CommandFailure
+}
+
+// waitForContinue waits for EOF to be received from fd. The parent process 
+// will close fd when this process may continue. 
+func waitForContinue(ctx context.Context, fd io.ReadCloser) error {
+	go func() {
+		<-ctx.Done()
+		if err := fd.Close(); err != nil {
+			logger.Errorf("closing continue pipe; err: %s", err)
+		}
+	}()
+
+	b := make([]byte, 1)
+	_, err := fd.Read(b)
+	if errors.Is(err, io.EOF) {
+		return nil
+	}
+	if err != nil {
+		return ierrors.Wrap(err)
+	}
+	return errExpectedEOF
+}
