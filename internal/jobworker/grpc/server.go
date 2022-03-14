fb3bc1fa79b966bbce0a6f90586cbdbb1f73c4f2
James <tjamesperry@hotmail.com>
2022-03-14 00:53:28 -0500

    job: updated grpc.JobWorker endpoints to check if user is owner of job before allowing access

--- internal/jobworker/grpc/server.go
+++ internal/jobworker/grpc/server.go
@@ -35,6 +35,11 @@ type JobWorker struct {
 }
 
 func (jw JobWorker) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResponse, error) {
+	user, ok := userFromContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "unauthenticated")
+	}
+
 	// TODO: ensure nil req.Command.Args are not being referenced.
 	valid := validator.New()
 	valid.AssertFunc(func() bool { return req.Command != nil }, "command empty")
@@ -45,7 +50,7 @@ func (jw JobWorker) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartR
 	}
 
 	j, err := job.New(
-		"owner",
+		user,
 		reexec.Command{
 			Name: req.GetCommand().Name,
 			Args: req.Command.Args,
@@ -53,12 +58,12 @@ func (jw JobWorker) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartR
 	)
 	if err != nil {
 		logger.Errorf("building job; error: %s", err)
-		return nil, status.Error(codes.Internal, "build job")
+		return nil, status.Error(codes.Internal, "error building job")
 	}
 
 	if err := jw.jobSvc.StartJob(ctx, *j); err != nil {
 		logger.Errorf("starting job; error: %s", err)
-		return nil, status.Errorf(codes.Internal, "start job")
+		return nil, status.Errorf(codes.Internal, "error starting job")
 	}
 
 	return &pb.StartResponse{
@@ -73,11 +78,16 @@ func (jw JobWorker) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartR
 }
 
 func (jw JobWorker) Stop(ctx context.Context, req *pb.StopRequest) (*pb.StopResponse, error) {
+	user, ok := userFromContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "unauthenticated")
+	}
+
 	if req.JobId != "" {
 		return nil, status.Error(codes.InvalidArgument, validator.Format("empty job ID"))
 	}
 
-	j, err := jw.fetchJob(ctx, req.JobId)
+	j, err := jw.fetchJob(ctx, user, req.JobId)
 	if err != nil {
 		return nil, err
 	}
@@ -87,18 +97,24 @@ func (jw JobWorker) Stop(ctx context.Context, req *pb.StopRequest) (*pb.StopResp
 	}
 
 	if err := jw.jobSvc.StopJob(ctx, j.ID); err != nil {
-		return nil, status.Error(codes.Internal, "stop job")
+		logger.Errorf("stop job; job: %s, error: %v", j.ID, err)
+		return nil, status.Error(codes.Internal, "error stopping job")
 	}
 
 	return &pb.StopResponse{}, nil
 }
 
 func (jw JobWorker) Status(ctx context.Context, req *pb.StatusRequest) (*pb.StatusResponse, error) {
+	user, ok := userFromContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "unauthenticated")
+	}
+
 	if req.JobId != "" {
 		return nil, status.Error(codes.InvalidArgument, validator.Format("empty job ID"))
 	}
 
-	j, err := jw.fetchJob(ctx, req.JobId)
+	j, err := jw.fetchJob(ctx, user, req.JobId)
 	if err != nil {
 		return nil, err
 	}
@@ -112,11 +128,16 @@ func (jw JobWorker) Status(ctx context.Context, req *pb.StatusRequest) (*pb.Stat
 }
 
 func (jw JobWorker) Output(req *pb.OutputRequest, stream pb.JobWorkerService_OutputServer) error {
+	user, ok := userFromContext(stream.Context())
+	if !ok {
+		return status.Error(codes.Unauthenticated, "unauthenticated")
+	}
+
 	if req.JobId != "" {
 		return status.Error(codes.InvalidArgument, validator.Format("empty job ID"))
 	}
 
-	j, err := jw.fetchJob(stream.Context(), req.JobId)
+	j, err := jw.fetchJob(stream.Context(), user, req.JobId)
 	if err != nil {
 		return err
 	}
@@ -125,17 +146,17 @@ func (jw JobWorker) Output(req *pb.OutputRequest, stream pb.JobWorkerService_Out
 	defer cancel()
 
 	// TODO: to buffer or not to buffer (buffer)
-	outputc := make(chan []byte)
+	outputc := make(chan []byte, streamBuffer)
 	go func() {
 		if err := j.StreamOutput(ctx, outputc); err != nil {
-			logger.Errorf("streaming output from job; error: %s", err)
+			logger.Errorf("streaming output from job; job: %s, error: %v", j.ID, err)
 		}
 		close(outputc)
 	}()
 
 	for b := range outputc {
 		if err := stream.Send(&pb.OutputResponse{Output: b}); err != nil {
-			logger.Errorf("streaming output to client; error: %s", err)
+			logger.Errorf("streaming output to client; job: %s, error: %s", j.ID, err)
 			return err
 		}
 	}
@@ -143,7 +164,7 @@ func (jw JobWorker) Output(req *pb.OutputRequest, stream pb.JobWorkerService_Out
 	return nil
 }
 
-func (jw JobWorker) fetchJob(ctx context.Context, jobID string) (*job.Job, error) {
+func (jw JobWorker) fetchJob(ctx context.Context, user string, jobID string) (*job.Job, error) {
 	id, err := uuid.Parse(jobID)
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, validator.Format("job ID not UUID"))
@@ -154,13 +175,24 @@ func (jw JobWorker) fetchJob(ctx context.Context, jobID string) (*job.Job, error
 		return nil, status.Error(codes.NotFound, "unknown job ID")
 	}
 	if err != nil {
-		return nil, status.Error(codes.Internal, "fetch job")
+		logger.Errorf("fetch job; job: %s, error: %v", id, err)
+		return nil, status.Error(codes.Internal, "error fetching job")
 	}
 
-	// TODO: retrieve owner from auth context
-	if j.Owner != "owner" {
-		return nil, status.Error(codes.PermissionDenied, "incorrect owner")
+	if j.Owner != user {
+		// Here we return codes.NotFound to prevent clients from determining that a
+		// job IDs exists without having access to it.
+		return nil, status.Error(codes.NotFound, "unknown job ID")
 	}
 
 	return j, nil
 }
+
+const (
+  // streamBuffer is the default stream buffer size. This is the number of
+  // chunks that may be held in memory.
+  streamBuffer = 16
+
+  // chunkSize is the size in bytes of each chunk to stream.
+  chunkSize = 128
+)
